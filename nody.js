911c8f346c56c67b830487919c1caf68d0143da7
csbaek <baek0429@hotmail.com>
2018-06-24 13:40:20 +0900

    Deleted unneccessary files.

--- nody.js
+++
@@ -1,8 +0,0 @@
-const request = require('request')
-
-request('https://booking.naver.com/booking/12/bizes/105398/items/2652591?area=bns', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-
-  console.log(res)
-});
-
