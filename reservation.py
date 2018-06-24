8dc9bf4dbfc43a5c3da3f3dcbd527035251e64ba
csbaek <baek0429@hotmail.com>
2018-06-24 23:02:19 +0900

    error fix

--- reservation.py
+++ reservation.py
@@ -19,54 +19,24 @@ courtNumbers = {
 	'7' : '2652591',
 	'8' : '2652592',}
 
-# chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
-browser = webdriver.Chrome(chrome_options=chrome_options, executable_path='chromedriver')
-browser.get(site)
-
-# for test only
-delay = 3 # seconds
-browser.implicitly_wait(delay)
-
-# wait until the page is loaded
-element = WebDriverWait(browser, 1).until(
-    lambda x: x.find_element_by_id('tel'))
-
-# get information(email,name,phonenumber) from reserveInfo
-email = browser.find_element_by_id('tel').send_keys(reserveInfo['phoneNumber'])
-email = browser.find_element_by_id('name').send_keys(reserveInfo['name'])
-username = browser.find_element_by_name('email').send_keys(reserveInfo['email'])
-
-# click the agree all button
-browser.find_element_by_class_name('chk_txt').click()
-
-# submit the form and return the result
-browser.find_element_by_class_name('btn').click()
-
 #Changes Monthly, hard to get it, serverside app.
 slotNumbers = '1234567'
 
-# court and time info
-site = "https://booking.naver.com/booking/12/bizes/105398/items/"+ courtNumbers['4'] + "/slots/" + slotNumber +"?area=bns"
-
-print "proceed reservation for" + site
-
 ## info class needed for reservation
-
 reserveInfo = {
 	'name' : 'test',
 	'email' : 'test@gmail.com',
 	'phoneNumber' : '010-3222-2223',
 	}
 
-chrome_options = Options()
+
+# court and time info
+site = "https://booking.naver.com/booking/12/bizes/105398/items/"+ courtNumbers['4'] + "/slots/" + slotNumber +"?area=bns"
+
 # chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 browser = webdriver.Chrome(chrome_options=chrome_options, executable_path='chromedriver')
 browser.get(site)
-delay = 3 # seconds
-
-browser.implicitly_wait(delay)
 
 # wait until the page is loaded
 element = WebDriverWait(browser, 1).until(
@@ -82,6 +52,3 @@ browser.find_element_by_class_name('chk_txt').click()
 
 # submit the form and return the result
 browser.find_element_by_class_name('btn').click()
-
-
-# Get Confirmation Number (if Success) or Reservation Fail Code
