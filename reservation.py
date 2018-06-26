e702e0e84d307561f16e0bfd2a664d0fd25365b5
csbaek <baek0429@hotmail.com>
2018-06-27 00:16:59 +0900

    info.txt file was added so that the program can read user input info.

--- reservation.py
+++ reservation.py
@@ -1,5 +1,6 @@
 ### Reseration Webpage info
 
+import fileread
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -7,45 +8,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-from urlparse import urlparse
 
 
-# Court Number Array
-courtNumbers = {
-	'3' : '2633373',
-	'4' : '2652587',
-	'5' : '2652589',
-	'6' : '2652590',
-	'7' : '2652591',
-	'8' : '2652592',}
-
-#Changes Monthly, hard to get it, serverside app.
-slotNumbers = '1234567'
-
-## info class needed for reservation
-reserveInfo = {
-	'name' : 'test',
-	'email' : 'test@gmail.com',
-	'phoneNumber' : '010-3222-2223',
-	}
-
+# getReservationInfo() from fileread module, returns array [court,time,name,email,mobile]
+infoArray = fileread.getReservationInfo('info.txt')
 
 # court and time info
-site = "https://booking.naver.com/booking/12/bizes/105398/items/"+ courtNumbers['4'] + "/slots/" + slotNumber +"?area=bns"
+url = "https://booking.naver.com/booking/12/bizes/105398/items/"+ infoArray[0] + "/slots/" + infoArray[1] +"?area=bns"
+
+
+# begin chrome options
+chrome_options = Options()
 
 # chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 browser = webdriver.Chrome(chrome_options=chrome_options, executable_path='chromedriver')
-browser.get(site)
+browser.get(url)
 
 # wait until the page is loaded
-element = WebDriverWait(browser, 1).until(
+element = WebDriverWait(browser, 5).until(
     lambda x: x.find_element_by_id('tel'))
 
 # get information(email,name,phonenumber) from reserveInfo
-email = browser.find_element_by_id('tel').send_keys(reserveInfo['phoneNumber'])
-email = browser.find_element_by_id('name').send_keys(reserveInfo['name'])
-username = browser.find_element_by_name('email').send_keys(reserveInfo['email'])
+mobile = browser.find_element_by_id('tel').send_keys(infoArray[4])
+email = browser.find_element_by_id('name').send_keys(infoArray[2])
+username = browser.find_element_by_name('email').send_keys(infoArray[3])
 
 # click the agree all button
 browser.find_element_by_class_name('chk_txt').click()
