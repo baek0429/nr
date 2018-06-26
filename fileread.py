0c96034a99787fc7d20e6998537b80c7385b5a15
csbaek <baek0429@hotmail.com>
2018-06-27 00:18:19 +0900

    Version 1.0.0.

---
+++ fileread.py
@@ -0,0 +1,32 @@
+import json
+
+
+# Court Number Array
+courtNumbers = {
+	'3' : '2633373',
+	'4' : '2652587',
+	'5' : '2652589',
+	'6' : '2652590',
+	'7' : '2652591',
+	'8' : '2652592',}
+
+# dictionary mirroring (change court and number)
+courtNumbers = {y:x for x,y in courtNumbers.iteritems()}
+
+# get read filename from same directory and  returns url(string)
+def getReservationInfo(fileName):
+	# file read
+	try:
+		f = open(fileName,'r')
+		contents = f.read()
+		infoArray = [x.strip() for x in contents.split(',')]
+		courtNumber = courtNumbers[infoArray[0]]
+		timeID = infoArray[1]
+		print("court number :" +  courtNumber + ", TimeID:"+ timeID)
+		return infoArray
+	except Exception as e:
+		print e
+
+
+if __name__ == "__main__":
+	getReservationInfo('info.txt')
